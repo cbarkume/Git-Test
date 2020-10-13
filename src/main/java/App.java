85204d07d298be62ed50b69a9003b23c2c120747
James Churchill <james@smashdev.com>
2020-10-13 12:30:14 -0700

    Update App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Succesful pull!!!");
+
+        System.out.println("After I drink coffee I show my empty mug to the IT guy and tell him I've successfully installed Java.");
+        System.out.println("He hates me.");
     }
 }
